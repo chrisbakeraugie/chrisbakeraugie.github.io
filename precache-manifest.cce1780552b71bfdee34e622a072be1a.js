bfa62b1f288326140835334ac63f9b2fae8f255f
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-02-04 12:41:13 -0500

    Updates

---
+++ precache-manifest.cce1780552b71bfdee34e622a072be1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700e096242d70ee3ed5ead18f6684b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "227c69791089e40a9844",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "72767251c43cca0fc7f5",
+    "url": "/static/css/main.db9f0080.chunk.css"
+  },
+  {
+    "revision": "227c69791089e40a9844",
+    "url": "/static/js/2.e1b6d9f7.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.e1b6d9f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72767251c43cca0fc7f5",
+    "url": "/static/js/main.15a6a5e5.chunk.js"
+  },
+  {
+    "revision": "a5522cd9e8c4180a4cfb",
+    "url": "/static/js/runtime-main.08893a90.js"
+  },
+  {
+    "revision": "f603e3b430491956b4d72a82455165be",
+    "url": "/static/media/AugustanaLogo.f603e3b4.jpg"
+  },
+  {
+    "revision": "650c99035e643cbc652d40d137619a65",
+    "url": "/static/media/Background.650c9903.jpg"
+  },
+  {
+    "revision": "0a390081ff012baf5b1225c0f6d95664",
+    "url": "/static/media/pagebackground.0a390081.jpg"
+  }
+]);
