c87ae239a0fd92d241a8835f85024e68f5091c1d
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2021-07-21 15:36:58 -0400

    Added simple welcome statement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,40 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 
 const Home = () => {
-    // const StyledHomePage = styled.div`
-    // width: 100vw;
-    // height: 85vh;
-    // background-color: turquoise;
-    // `;
+    const StyledWelcomeDiv = styled.div`
+    position:absolute;
+    height: 30vh;
+    width:30vw;
+    top:35vh;
+    left:35vw;
+    text-align:center;
+    `;
 
     return (
         <div id="home-container">
-        <div id="the-sun"></div>
-        <div id="rectangle1"></div>
-        <div id="rectangle2"></div>
-        <div id="trapez3"></div>
-        <div id="rectangle4"></div>
-        <div id="trapez5"></div>
-        <div id="rectangle6"></div>
-        <div id="rectangle7"></div>
-        <div id="rectangle8"></div>
-        <div id="rectangle9"></div>
-        <div id="rectangle10"></div>
-        <div id="rectangle11"></div>
-        <div id="rectangle12"></div>
-        
+            <div id="the-sun"></div>
+            <div id="rectangle1"></div>
+            <div id="rectangle2"></div>
+            <div id="trapez3"></div>
+            <div id="rectangle4"></div>
+            <div id="trapez5"></div>
+            <div id="rectangle6"></div>
+            <div id="rectangle7"></div>
+            <div id="rectangle8"></div>
+            <div id="rectangle9"></div>
+            <div id="rectangle10"></div>
+            <div id="rectangle11"></div>
+            <div id="rectangle12"></div>
+            <StyledWelcomeDiv>
+                <h1>Hi, my name is Chris.
+            I'm a software developer</h1>
+            </StyledWelcomeDiv>
+
 
         </div>
-        
+
     );
 }
 
