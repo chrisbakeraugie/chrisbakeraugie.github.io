abe8ee6edea1dfe8229abbf2234568000e3ce06c
Chris Baker <chrisbaker@martins-imac.myfiosgateway.com>
2023-12-30 13:08:18 -0500

    Formatted the slider

--- src/components/StyledSlider.jsx
+++ src/components/StyledSlider.jsx
@@ -1,27 +1,62 @@
-import { Slider } from '@mui/material'
+import { Slider, SliderThumb } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import Brightness4OutlinedIcon from '@mui/icons-material/Brightness4Outlined'
+import { useContext } from 'react'
+import { AppContext } from '../context'
 
-const MUIStyledSlider = styled(Slider)(({ theme }) => ({
-	position: 'fixed',
-	transform: 'translateX(-50%)',
-	left: '50%',
-	[theme.breakpoints.up('lg')]: {
-		bottom: 20,
-		width: '90%',
-		maxWidth: '1400px',
-	},
-	[theme.breakpoints.between('sm', 'lg')]: {
-		bottom: 20,
-		width: '80%',
-	},
-	[theme.breakpoints.down('sm')]: {
-		bottom: 10,
-		width: '70%',
-	},
-}))
+function SliderThumbComponent(props) {
+	const { children, ...other } = props
+	return (
+		<SliderThumb {...other}>
+			{children}
+			<Brightness4OutlinedIcon />
+		</SliderThumb>
+	)
+}
+
+const MUIStyledSlider = styled(Slider)(
+	({ theme, strokeColor, backgroundColor }) => ({
+		color: strokeColor,
+		height: 3,
+		padding: '13px 0',
+		'& .MuiSlider-thumb': {
+			height: 27,
+			width: 27,
+			backgroundColor,
+			border: '1px solid currentColor',
+			'&:hover': {
+				boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+			},
+		},
+		position: 'fixed',
+		transform: 'translateX(-50%)',
+		left: '50%',
+		[theme.breakpoints.up('lg')]: {
+			bottom: 20,
+			width: '90%',
+			maxWidth: '1400px',
+		},
+		[theme.breakpoints.between('sm', 'lg')]: {
+			bottom: 20,
+			width: '80%',
+		},
+		[theme.breakpoints.down('sm')]: {
+			bottom: 10,
+			width: '70%',
+		},
+	})
+)
 
 const StyledSlider = (props) => {
-	return <MUIStyledSlider {...props} />
+	const { strokeColor, backgroundColor } = useContext(AppContext)
+	return (
+		<MUIStyledSlider
+			{...props}
+			backgroundColor={backgroundColor}
+			slots={{ thumb: SliderThumbComponent }}
+			strokeColor={strokeColor}
+		/>
+	)
 }
 
 export default StyledSlider
--- src/js/interpolateColor.js
+++ src/js/interpolateColor.js
@@ -1,6 +1,6 @@
 function interpolateColor(color1, color2, factor) {
 	return {
-		r: Math.round(color1.r + factor * (color2.r - color1.r)),
+		r: Math.round(color1.r + factor * (color2.r - color1.r)), // "Interpolating" the colors, weighting theme by a factor
 		g: Math.round(color1.g + factor * (color2.g - color1.g)),
 		b: Math.round(color1.b + factor * (color2.b - color1.b)),
 		a: color1.a + factor * (color2.a - color1.a),
